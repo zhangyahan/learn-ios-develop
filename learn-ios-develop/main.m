6ac6337173d92d7e7d1730cfe1d5ae42666a1b9c
ZhangYahan <yahan233@163.com,zyahan1997@163.com>
2022-10-28 22:43:21 +0800

    Hello World.

--- learn-ios-develop/main.m
+++ learn-ios-develop/main.m
@@ -4,12 +4,20 @@
 //
 //  Created by 张亚瀚 on 2022/10/28.
 //
-
+/**
+ Objective-C 语言学习，OC语言为C语言的扩展。所以需要具备一定C语言的知识。
+ 如果你不了解C语言但是熟悉其他面向编程语言，如：Java，那么也可以直接学习OC。
+ 我学习这门语言的主要用途为完成IOS应用的开发。
+ */
 #import <Foundation/Foundation.h>
 
+/**
+ 主函数
+ */
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        // 打印 Hello World
+        // OC 依赖 Foundation 框架，Foundation 框架内包装了很多数据类型用于简化C语言
         NSLog(@"Hello, World!");
     }
     return 0;
