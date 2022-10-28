6d90309d715fc8a2095fa42cecb64f5e8fab13c7
ZhangYahan <yahan233@163.com,zyahan1997@163.com>
2022-10-28 22:34:58 +0800

    Initial Commit

---
+++ learn-ios-develop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learn-ios-develop
+//
+//  Created by 张亚瀚 on 2022/10/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
